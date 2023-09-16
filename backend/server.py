7f36a494292ab72043581a01a4f8b4cb8bf7c5e4
nepthius <anishdshekar@gmail.com>
2023-09-16 19:21:22 -0400

    audio without wav conversion

--- backend/server.py
+++ backend/server.py
@@ -1,10 +1,78 @@
-from flask import Flask
+from flask import Flask, request, jsonify
+from werkzeug.utils import secure_filename
+import os
+import datetime
+import traceback
+
 
 app = Flask(__name__)
 
 @app.route("/voiceanalysis")
 def voiceanalysis():
-    return {"voice":["sad", "happy", "angry"]}
+    return [{"sad":"22%"}, {"happy":"50%"}]
+
+
+
+UPLOAD_FOLDER = os.path.abspath(os.path.dirname(__file__)) + '/audio'
+ALLOWED_EXTENSIONS = {'ogg', 'mp3', 'wav', 'm4a'}
+def allowed_file(filename):
+    return '.' in filename and \
+           filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+
+from pydub import AudioSegment
+
+@app.route('/upload', methods=['POST'])
+def upload_file():
+    # Check if the request has an 'audio' file part
+    if 'audio' not in request.files:
+        app.logger.error("No audio file found in request")
+        return jsonify({"error": "No audio file found"}), 400
+    
+    file = request.files['audio']
+
+    # Check if the user did not select a file or if it's not allowed
+    if file.filename == '' or not allowed_file(file.filename):
+        app.logger.error("Invalid file name or format")
+        return jsonify({"error": "Invalid file"}), 400
+
+    if file:
+        timestamp = datetime.datetime.now().strftime('%Y%m%d%H%M%S')
+
+        # Create a temporary filename for the uploaded file
+        temp_filename = os.path.join(UPLOAD_FOLDER, "temp_audio." + file.filename.rsplit('.', 1)[1].lower())
+        file.save(temp_filename)
+
+        output_filename = os.path.join(UPLOAD_FOLDER, f"audio_{timestamp}.wav")
+
+        try:
+            # Determine the audio format from the file extension and then load using pydub
+            extension = file.filename.rsplit('.', 1)[1].lower()
+            if extension == "ogg":
+                audio = AudioSegment.from_ogg(temp_filename)
+            elif extension == "mp3":
+                audio = AudioSegment.from_mp3(temp_filename)
+            elif extension == "wav":
+                audio = AudioSegment.from_wav(temp_filename)
+            elif extension == "m4a":
+                audio = AudioSegment.from_file(temp_filename, format="m4a")
+            else:
+                raise ValueError("Unsupported audio format")
+
+            audio.export(output_filename, format="wav")
+
+            # Clean up the temporary file
+            os.remove(temp_filename)
+
+            return jsonify({"message": f"File uploaded and converted successfully as {output_filename}!"}), 200
+
+        except Exception as e:
+            app.logger.error(f"Error during conversion: {str(e)}")
+            app.logger.error(traceback.format_exc())  # Log the full traceback for better debugging
+            return f"Error converting the audio: {str(e)}", 500
+
+    else:
+        app.logger.error("No file uploaded")
+        return "No file uploaded!", 400
 
 if __name__ == "__main__":
     app.run(debug=True)
--- frontend/src/pages/product.js
+++ frontend/src/pages/product.js
@@ -1,33 +1,131 @@
-import React, {useState, useEffect} from 'react'
+import React, { useState, useRef, useEffect } from 'react';
+import { useSpring, animated } from 'react-spring';
 
 const Product = () => {
-    const [data, setData] = useState([{}])
+    const [audioFile, setAudioFile] = useState(null);
+    const [isRecording, setIsRecording] = useState(false);
+    const [mediaRecorder, setMediaRecorder] = useState(null);
+    const [chunks, setChunks] = useState([]);
+    let audioChunks = [];
+    const mediaRecorderRef=useRef(null)
 
-    useEffect(()=> {
-        fetch("/voiceanalysis").then(
-            res => res.json()
-        ).then(
-            data=> {
-                setData(data)
-            }
-        )
-    })
+    
+    const fadeIn = useSpring({
+        opacity: 1,
+        from: { opacity: 0 }
+    });
 
-    return(
-        <div>
-            
-            
-            {(typeof data.voice==='undefined') ? (
-                <p>Loading...</p>
-            ):(
-                data.voice.map((v, i)=>(
-                    <p key={i}>{v}</p>
-                ))
-            )}
+    const sendAudioToServer = (audioBlob) => {
+        console.log("audioblob: ", audioBlob)
+        const blobUrl = URL.createObjectURL(audioBlob);
+        const a = document.createElement('a');
+        a.style.display = 'none';
+        a.href = blobUrl;
+        a.download = 'test.ogg';
+        document.body.appendChild(a);
+        a.click();
+        const formData = new FormData();
+        formData.append('audio', audioBlob, 'audioRecording.ogg');
+        fetch('/upload', {
+            method: 'POST',
+            body: formData
+        })
+        .then(response => response.json())
+        .then(data => {
+            console.log(data);
+        })
+        .catch(error => {
+            console.error("There was an error sending the audio file", error);
+        });
+    };
 
+
+    const handleFileUpload = (event) => {
+        const file = event.target.files[0];
+        if (file && file.type.startsWith('audio/')) {
+            setAudioFile(file);
             
-        </div>
-    )
+            // Extract the original name and use it when appending to formData
+            const formData = new FormData();
+            formData.append('audio', file, file.name);
+
+            fetch('/upload', {
+                method: 'POST',
+                body: formData
+            })
+            .then(response => response.json())
+            .then(data => {
+                console.log(data);
+            })
+            .catch(error => {
+                console.error("There was an error sending the audio file", error);
+            });
+        }
+    };
+
+
+    const startRecording = () => {
+        navigator.mediaDevices.getUserMedia({ audio: true }).then(stream => {
+            const recorder = new MediaRecorder(stream);
+            
+            recorder.ondataavailable = e => {
+                console.log("Data available:", e.data);
+                audioChunks.push(e.data);
+            };
+
+            recorder.onstop = () => {
+                console.log("On stop triggered");
+                const blob = new Blob(audioChunks, { 'type' : 'audio/ogg; codecs=opus' });
+                sendAudioToServer(blob);
+                setAudioFile(blob);
+                setIsRecording(false);
+                audioChunks = [];
+
+                // Use the useRef to access the stream and cleanup
+                const tracks = mediaRecorderRef.current.stream.getTracks();
+                tracks.forEach(track => track.stop());
+            };
+
+            recorder.start();
+
+            mediaRecorderRef.current = recorder;  // <-- Set the ref to the recorder
+            setIsRecording(true);
+        });
+    };
+
+    const stopRecording = () => {
+        if (mediaRecorderRef.current && mediaRecorderRef.current.state === 'recording') {
+            mediaRecorderRef.current.stop();
+            console.log("Stopped recording");
+        }
+    };
+
+    return (
+        <animated.div style={fadeIn} className="product-dashboard">
+            <div className="graph-container">
+                {!audioFile && <p>Upload or record an audio file to view the analysis graph.</p>}
+                {/* The graph component will be rendered here once the audio file is uploaded/recorded */}
+            </div>
+
+            <div className="audio-actions">
+                {!isRecording ? (
+                    <>
+                        <label className="upload-button">
+                            Upload Audio File
+                            <input type="file" accept="audio/*" onChange={handleFileUpload} hidden />
+                        </label>
+                        <button className="record-button" onClick={startRecording}>
+                            Start Recording
+                        </button>
+                    </>
+                ) : (
+                    <button className="record-button stop" onClick={stopRecording}>
+                        Stop Recording
+                    </button>
+                )}
+            </div>
+        </animated.div>
+    );
 }
 
-export default Product
+export default Product;
